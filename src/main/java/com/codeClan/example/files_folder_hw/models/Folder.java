5c9e9b900785f5c5326378273d4d930a0307f695
belus90 <belus90@gmail.com>
2022-05-24 16:49:32 +0100

    set up models: folder

--- src/main/java/com/codeClan/example/files_folder_hw/models/Folder.java
+++ src/main/java/com/codeClan/example/files_folder_hw/models/Folder.java
@@ -1,4 +1,49 @@
 package com.codeClan.example.files_folder_hw.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
